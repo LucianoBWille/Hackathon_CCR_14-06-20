cfb99d8b9ec24e01a2c6f4d75378c8572b4ad35e
LucianoBWille <00lucianobwille00@gmail.com>
2020-06-14 18:21:37 -0300

    Initial Commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,46 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <div class="q-pa-md flex flex-center">
+      <q-btn round size="0px" margin="0px" padding="0px" outline="true">
+        <q-circular-progress
+          show-value
+          font-size="10px"
+          class="q-ma-md"
+          :value="value"
+          size="50px"
+          :thickness="0.25"
+          color="primary"
+          track-color="grey-3"
+        >
+          <q-avatar size="40px">
+            <img src="https://cdn.quasar.dev/logo/svg/quasar-logo.svg">
+          </q-avatar>
+        </q-circular-progress>
+      </q-btn>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  name: 'LayoutDefault',
+
+  data () {
+    return {
+      value: 10,
+      categorias:[
+        {
+          name: 'Alimentação', 
+          percentual: 75, 
+          icone: "https://img.icons8.com/plasticine/100/000000/food.png"
+        }
+      ]
+    }
   }
 }
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style lang="scss" scoped>
+  
 </style>
